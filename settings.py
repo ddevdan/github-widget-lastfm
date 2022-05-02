7def19e18fcce41ae30736d22225fb544b0f5c44
Daniel Carvalho <dco3@cin.ufpe.br>
2022-05-02 02:55:02 -0300

    fix change variable name

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from decouple import config
 
 
-LAST_FM_BASE_URL = config("LAST_FM_BASE_URL")
+LAST_FM_API_KEY = config("LAST_FM_API_KEY")
+LAST_FM_BASE_URL = f"https://ws.audioscrobbler.com/2.0/?api_key={LAST_FM_API_KEY}&method=user.getrecenttracks&format=json"
